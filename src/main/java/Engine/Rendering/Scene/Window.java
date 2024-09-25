99910fc2b75f108ffd448ae9a2b207182e91d032
TMG <66220144+TMG8047KG@users.noreply.github.com>
2024-09-25 22:23:42 +0300

    Things

--- src/main/java/Engine/Rendering/Scene/Window.java
+++ src/main/java/Engine/Rendering/Scene/Window.java
@@ -35,7 +35,7 @@ public class Window {
             glfwWindowHint(GLFW_SAMPLES, 4);
         }
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
         if (opts.compatibleProfile) {
             glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
         } else {
